5059d0b77b3a74b9420e5c866adb6d8d83cc9670
yeom <yeomsh03@gmail.com>
2026-07-15 12:54:54 +0900

    이미지 테스트1

--- backend/src/main/java/com/nyo/domain/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/nyo/domain/comment/repository/CommentRepository.java
@@ -3,12 +3,12 @@ package com.nyo.domain.comment.repository;
 import com.nyo.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface    CommentRepository extends JpaRepository<Comment, Long> {
+import java.util.List;
+import java.util.Optional;
 
-    /**
-     * 이 레포지토리가 어떤 엔티티를 다루는지 스스로 설명합니다.
-     */
-    default String introduce() {
-        return "CommentRepository: Comment 엔티티에 대한 기본 CRUD를 제공합니다. (구현 예정)";
-    }
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPostIdAndIsDeletedOrderByCreatedAtAsc(Long postId, Integer isDeleted);
+
+    Optional<Comment> findByIdAndIsDeleted(Long id, Integer isDeleted);
 }
